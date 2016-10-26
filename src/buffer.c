e69bb44e855663ac39022fef36a2f1ffe6d62fdc
Gustavo Dutra <gobannondutra@gmail.com>
2016-10-26 11:45:25 -0200

    Preparando a estrutura interna de produtor-consumidor no buffer

--- src/buffer.c
+++ src/buffer.c
@@ -20,6 +20,7 @@
 #include <assert.h>
 #include <buffer.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <util.h>
 
 /*
@@ -33,6 +34,8 @@ struct buffer
 	unsigned last;  /* Last element in the buffer.  */
 	pthread_mutex_t mutex_first;
 	pthread_mutex_t mutex_last;
+	sem_t empty;
+	sem_t full;
 };
 
 /*
@@ -56,6 +59,9 @@ struct buffer *buffer_create(unsigned size)
 	pthread_mutex_init(&buf->mutex_first, NULL);
 	pthread_mutex_init(&buf->mutex_last, NULL);
 
+	sem_init(&buf->full, 0, 0);
+	sem_init(&buf->empty, 0, size);
+
 	return (buf);
 }
 
@@ -73,6 +79,9 @@ void buffer_destroy(struct buffer *buf)
 	pthread_mutex_destroy(&buf->mutex_first);
 	pthread_mutex_destroy(&buf->mutex_last);
 
+	sem_destroy(&buf->empty);
+	sem_destroy(&buf->full);
+
 	free(buf);
 }
 
@@ -85,6 +94,8 @@ void buffer_put(struct buffer *buf, unsigned item)
 	assert(buf != NULL);
 
 	/* Expand buffer. */
+
+	sem_wait(&buf->empty);
 	pthread_mutex_lock(&buf->mutex_last);
 
 	if (buf->last == buf->size)
@@ -95,6 +106,7 @@ void buffer_put(struct buffer *buf, unsigned item)
 	buf->data[buf->last++] = item;
 
 	pthread_mutex_unlock(&buf->mutex_last);
+	sem_post(&buf->empty);
 }
 
 /*
@@ -107,9 +119,11 @@ unsigned buffer_get(struct buffer *buf)
 	/* Sanity check. */
 	assert(buf != NULL);
 	
+	sem_wait(&buf->full);
 	pthread_mutex_lock(&buf->mutex_first);
 	item = buf->data[buf->first++];
 	pthread_mutex_unlock(&buf->mutex_first);
+	sem_post(&buf->empty);
 
 	return (item);
 }
